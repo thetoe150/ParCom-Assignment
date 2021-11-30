71042c8951e8b987dda51433adb71cc9884c0b83
thetoe150 <74317369+thetoe150@users.noreply.github.com>
2021-11-30 15:03:45 +0700

    Delete SystemMonitoring.h

--- SystemMonitoring.h
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-TCHAR *PrintProcessName(HANDLE hProcess);
-void PrintModules(HANDLE hProcess);
-void PrintMemoryInfo(HANDLE hProcess);
-void PrintFileIOInfo(HANDLE hProcess);
-void PrintCPUusageInfo(DWORD processID);
-void PrintSystemInfo();
-int NumThreadsOfProcess(DWORD processID);
